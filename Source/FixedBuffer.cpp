0028b783cb20c19b0ac185e33822ad9135755ffc
dpezdirc <105217062+dpezdirc@users.noreply.github.com>
2026-01-03 21:02:39 +0100

    Add RepeatingBuffer

--- Source/FixedBuffer.cpp
+++ Source/FixedBuffer.cpp
@@ -10,7 +10,6 @@ FixedBuffer::FixedBuffer(int size) :
 
 void FixedBuffer::Reset()
 {
-    m_readIdx = 0;
     m_writeIdx = 0;
 }
 
@@ -33,16 +32,55 @@ void FixedBuffer::WriteSamples(const float* samples, int nSamples)
     }
 }
 
-float FixedBuffer::ReadSample()
+float FixedBuffer::GetSample(int idx)
 {
-    const float sample = m_data[m_readIdx];
+    assert(idx >= 0 && idx < m_size);
 
-    m_readIdx = (m_readIdx + 1) % m_size;
-
-    return sample;
+    return m_data[idx];
 }
 
 bool FixedBuffer::IsFull() const
 {
     return m_writeIdx == m_size;
+}
+
+RepeatingBuffer::RepeatingBuffer(int maxSize, int size) :
+	m_fixedBuffer(maxSize),
+	m_size(size)
+{
+}
+
+void RepeatingBuffer::Reset()
+{
+	m_fixedBuffer.Reset();
+	m_readIdx = 0;
+}
+
+void RepeatingBuffer::SetSize(int newSize)
+{
+    assert(newSize > 0 && newSize <= m_fixedBuffer.GetSize());
+
+    m_size = newSize;
+
+    if (m_readIdx >= m_size)
+        m_readIdx = 0;
+}
+
+void RepeatingBuffer::WriteSample(float sample)
+{
+	m_fixedBuffer.WriteSample(sample);
+}
+
+void RepeatingBuffer::WriteSamples(const float* samples, int nSamples)
+{
+	m_fixedBuffer.WriteSamples(samples, nSamples);
+}
+
+float RepeatingBuffer::ReadSample()
+{
+	const float sample = m_fixedBuffer.GetSample(m_readIdx);
+
+	m_readIdx = (m_readIdx + 1) % m_size;
+
+	return sample;
 }
--- Source/FixedBuffer.h
+++ Source/FixedBuffer.h
@@ -11,7 +11,8 @@ public:
 	void Reset();
 	void WriteSample(float sample);
 	void WriteSamples(const float* samples, int nSamples);
-	float ReadSample();
+	float GetSample(int idx);
+	int GetSize() { return m_size; }
 
 private:
 	bool IsFull() const;
@@ -19,6 +20,23 @@ private:
 private:
 	std::vector<float> m_data;
 	int m_size = 0;
-	int m_readIdx = 0;
 	int m_writeIdx = 0;
+};
+
+class RepeatingBuffer
+{
+public:
+	RepeatingBuffer(int maxSize, int size);
+	RepeatingBuffer() : RepeatingBuffer(0, 0) {}
+
+	void Reset();
+	void SetSize(int newSize);
+	void WriteSample(float sample);
+	void WriteSamples(const float* samples, int nSamples);
+	float ReadSample();
+
+private:
+	FixedBuffer m_fixedBuffer;
+	int m_size = 0;
+	int m_readIdx = 0;
 };
